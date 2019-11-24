f13282492fb97b76026fdb019463b8a43a1b8e35
Flora Vuong Nu Dong <floravuong@Floras-MacBook-Pro-2.local>
2019-11-24 18:36:09 +0800

    reynt að setja inn fyrirlestrarnir og smá css

--- src/lib/list.js
+++ src/lib/list.js
@@ -1,11 +1,31 @@
-import { empty } from './helpers';
+import {
+  empty
+} from './helpers';
 
 export default class List {
   constructor() {
     this.container = document.querySelector('.list');
   }
-
-  load() {
-    empty(this.container);
-  }
 }
+
+//  Búa til listi fyrir fyrirlestrarnir
+export function fetchJSONFile(path, callback) {
+  var httpRequest = new XMLHttpRequest();
+  httpRequest.onreadystatechange = function () {
+    if (httpRequest.readyState === 4) {
+      if (httpRequest.status === 200) {
+        var data = JSON.parse(httpRequest.responseText);
+        if (callback) callback(data);
+      }
+    }
+  };
+  httpRequest.open('GET', path);
+  httpRequest.send();
+}
+
+// this requests the file and executes a callback with the parsed result once
+//   it is available
+fetchJSONFile('../../lectures.json', function (data) {
+  // do something with your data
+  console.log(data);
+});
